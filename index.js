b8a63153888f99a8076db4687fafcb720708b9c4
AxelHuon <81225848+AxelHuon@users.noreply.github.com>
2022-12-07 13:17:37 +0100

    init

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+const http = require('http')
+const cors = require('cors')
+const {Server} = require("socket.io")
+
+app.use(cors())
+
+
+const server = http.createServer(app)
+const io = new Server(server, {
+	cors :{
+		origin : "http://localhost:3000",
+		methods : ["GET, POST"]
+	}
+})
+
+
+io.on('connection' , socket => {
+
+
+
+	socket.on("join_room" , (data)=>{
+		socket.join(data.id);
+		console.log(`le user ${socket.id} a joint la room ${data.id}`)
+	})
+
+
+	socket.on('send_message' ,(data)=>{
+		socket.to(data.room).emit("receive_message", data)
+		console.log(data)
+	})
+})
+
+
+
+
+server.listen(3001, () =>{
+	console.log("Server Running")
+})
